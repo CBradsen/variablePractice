76311f40c50bfa9f9246713ad80bb65a13d9c072
Carol Bradsen <carolbradsen@gmail.com>
2022-11-08 19:30:27 -0700

    Add booleans

--- variables.js
+++ variables.js
@@ -1,3 +1,11 @@
 var name = “Elizabeth”
 var funny = “Calls a swimsuit a Bookini”
 var cat = “Squiggles is a cat”
+var age = 1
+var e = 6
+var s = 5.9
+var isElizabethACat = false
+var bookiniASwimsuit = true
+var squigglesIsACat = true
+
+
